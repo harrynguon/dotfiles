4f67c2511be12a151dfe9975c1a094209cb50c5a
Harry Nguon <harrynguon@gmail.com>
2018-09-23 02:57:31 +1200

    Added ~/.local/bin to path for Pip application installations

---
+++ config.fish
@@ -0,0 +1,4 @@
+export ANDROID_HOME=$HOME/Android/Sdk
+set -x PATH $PATH $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools ~/.local/bin
+
+cat ~/.cache/wal/sequences
